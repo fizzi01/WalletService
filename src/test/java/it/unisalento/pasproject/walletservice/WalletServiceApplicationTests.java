8a05270e32cf9c0e8d8c783335d9d50210551628
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-05-10 17:07:59 +0200

    Parameterized app properties Fixed mqtt topics

--- src/test/java/it/unisalento/pasproject/walletservice/WalletServiceApplicationTests.java
+++ src/test/java/it/unisalento/pasproject/walletservice/WalletServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package it.unisalento.pasproject.walletservice;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+import it.unisalento.pasproject.walletservice.WalletServiceApplication;
+import it.unisalento.pasproject.walletservice.repositories.WalletRepository;
+import it.unisalento.pasproject.walletservice.service.WalletService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WalletServiceApplicationTests {
 
+    @Mock
+    private WalletRepository walletRepository;
+
+    @InjectMocks
+    private WalletServiceApplication walletServiceApplication;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
-    void contextLoads() {
+    void shouldReturnZeroBalance() {
+        when(walletRepository.findByEmail("newUser").isPresent()).thenReturn(null);
+        double balance = walletRepository.findByEmail("newUser").get().getBalance();
+        assertThat(balance).isEqualTo(0.0);
     }
 
 }
